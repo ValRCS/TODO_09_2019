1115d02f317673397caaedd6896ed4a41c5377e1
Valdis Saules <valdis.s.coding@gmail.com>
2019-09-26 11:29:16 +0300

    Refactored with app container and main function

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,38 @@
 console.log("Started TODO app!");
+main();
 
-const mybtn = document.querySelector("#btn1");
 
-//we create an event handler for add button
-mybtn.onclick = (event) => {
-    console.log("You pressed Add!");
-    const inputField = document.querySelector("#myinput");
-    console.log("Input value is: " + inputField.value);
+function main() {
+    const app = {
+        lastId : 0,
+        mybtn : document.querySelector("#btn1"),
+        todoCont : document.querySelector("#todos-cont")
+    }
 
-    //Add New TODO 
-    const todoCont = document.querySelector("#todos-cont");
+    //we create an event handler for add button
+    app.mybtn.onclick = (event) => {
+        console.log("You pressed Add!");
+        const inputField = document.querySelector("#myinput");
+        console.log("Input value is: " + inputField.value);
 
-    addTodo(todoCont, inputField.value);
+        //Add New TODO 
+        addTodo(app.todoCont, inputField.value);
+    }
 }
 
+
 function addTodo(parent, value) {
+    //Create New Todo
     const newTodo = document.createElement("div");
-    newTodo.innerText = value;   
+    newTodo.innerHTML = `
+        <input type="checkbox" name="" id="j-chk-1">
+        <span class="job-desc" id="j-desc-1">Buy Milk</span>
+        <button class="del-btn" id="j-del-1">DELETE</button>
+         `;   
     parent.appendChild(newTodo);
+
+
+    //add Handlers
+    //TODO 
 }
 
